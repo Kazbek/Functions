8f9bc3edc465a4aec22aac5d4b0e3ac0bef3816c
dkazbek <kazbek.se@gmail.com>
2017-10-22 17:11:02 +0300

    Первый тест для границы интервала

---
+++ Functions.Tests/InrervalEdges/IntervalEdge/InstanceCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Functions.Implementations.Intervals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Functions.Tests.InrervalEdges.IntervalEdge
+{
+    [TestClass]
+    public class InstanceCreate
+    {
+        [TestMethod]
+        [DataRow(-233,false)]
+        [DataRow(-1, true)]
+        [DataRow(0,true)]
+        [DataRow(1,false)]
+        [DataRow(int.MaxValue,true)]
+        [DataRow(int.MinValue,false)]
+        public void IntFromDatrow(int value, bool inclusive)
+        {
+            IntervalEdge<int> edge = new IntervalEdge<int>(value, inclusive);
+            Assert.AreEqual(edge.Position, value);
+            Assert.AreEqual(edge.Inclusive, inclusive);
+        }
+
+        [TestMethod]
+        public void IntForce()
+        {
+            IntervalEdge<int> edge = new IntervalEdge<int>(3, true);
+            Assert.AreEqual(edge.Position, 3);
+            Assert.AreEqual(edge.Inclusive, true);
+        }
+
+    }
+}
