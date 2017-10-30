96161e536d2733535103b3969fe90b2a6a86fb4f
dkazbek <kazbek.se@gmail.com>
2017-10-30 18:17:05 +0300

    Почистил список using директив

--- Functions.Tests/Functions/Composite/InstanceCreate.cs
+++ Functions.Tests/Functions/Composite/InstanceCreate.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Functions.Implementations.Functions;
 using Functions.Implementations.Intervals;
 using Functions.Interfaces;
